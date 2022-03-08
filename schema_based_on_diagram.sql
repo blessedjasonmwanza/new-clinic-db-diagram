808fea423e925c3db469fba8d65ba687f365d578
Natalia Poletaeva <natfly799@gmail.com>
2022-03-08 19:50:47 +0500

    Finish creating tables and add several foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,18 +1,22 @@
 DROP TABLE IF EXISTS medical_histories;
 
+CREATE TABLE patients(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR,
+    date_of_birth DATE
+);
+
+CREATE TABLE treatments(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR,
+    name VARCHAR
+);
+
 CREATE TABLE medical_histories(
-    id INT PRIMARY KEY AUTO GENERATED AS IDENTITY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     admitted_at TIMESTAMP DEFAULT NOW(),
     patient_id INT,
     status VARCHAR
-);
-
-DROP TABLE IF EXISTS patients;
-
-CREATE TABLE patients(
-    id INT PRIMARY KEY AUTO GENERATED AS IDENTITY,
-    name VARCHAR,
-    date_of_birth DATE
 );
 ALTER TABLE medical_histories ADD CONSTRAINT patient_id_fk FOREIGN KEY (patient_id) REFERENCES patients(id);
 
@@ -23,8 +27,15 @@ CREATE TABLE invoices(
     payed_at TIMESTAMP,
     medical_history_id INT
 );
+ALTER TABLE invoices ADD CONSTRAINT invoices_medical_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
 
-ALTER TABLE invoices ADD CONSTRAINT invoices_medical_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
-
-
--- ALTER TABLE invoices ADD CONSTRAINT invoices_fk FOREIGN KEY (id) REFERENCES invoice_items(id);
+CREATE TABLE invoice_items(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treatment_id INT
+);
+ALTER TABLE invoice_items ADD CONSTRAINT invoice_id_fk FOREIGN KEY (invoice_id) REFERENCES invoices(id);
+ALTER TABLE invoice_items ADD CONSTRAINT treatment_id_fk FOREIGN KEY (treatment_id) REFERENCES treatments(id);
